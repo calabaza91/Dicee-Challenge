ce744d9ea58ee09f9a26d312b940ab4de55f8a7e
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-14 15:16:50 -0600

    Change img using the DOM
    Using setAttribute() and string concatenation, make player 1's dice value the same as randNum variable

--- dicee.js
+++ dicee.js
@@ -2,3 +2,6 @@
 
 // Random number 1 - 6
 var randNum = Math.floor(Math.random() * 6) + 1;
+
+// Make Player 1 die's value equal to random number
+document.querySelector(".img1").setAttribute("src", "images/dice" + randNum + ".png");
