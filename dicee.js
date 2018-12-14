19ceff64217031ab9f38bf3e7854f081dce43089
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-14 15:20:18 -0600

    Make player 2's dice work like player 1's dice

--- dicee.js
+++ dicee.js
@@ -1,7 +1,9 @@
 //JavaScript file for Dicee Challenge
 
-// Random number 1 - 6
-var randNum = Math.floor(Math.random() * 6) + 1;
+//2 Random numbers 1 - 6
+var randNum1 = Math.floor(Math.random() * 6) + 1;
+var randNum2 = Math.floor(Math.random() * 6) + 1;
 
-// Make Player 1 die's value equal to random number
-document.querySelector(".img1").setAttribute("src", "images/dice" + randNum + ".png");
+// Make Player 1 and 2's dice value equal to complementary random number
+document.querySelector(".img1").setAttribute("src", "images/dice" + randNum1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randNum2 + ".png");
