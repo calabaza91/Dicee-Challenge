4ae5e1f71deaac78bc30dcfd9d7f3d8e982b87d5
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-15 00:19:35 -0600

    Update dicee.js
    When a player wins or if there is a draw, change h1 to show message

--- dicee.js
+++ dicee.js
@@ -7,3 +7,12 @@ var randNum2 = Math.floor(Math.random() * 6) + 1;
 // Make Player 1 and 2's dice value equal to complementary random number
 document.querySelector(".img1").setAttribute("src", "images/dice" + randNum1 + ".png");
 document.querySelector(".img2").setAttribute("src", "images/dice" + randNum2 + ".png");
+
+//Change h1 depending on winner or draw
+if (randNum1 > randNum2) {
+  document.querySelector("h1").innerText = "🏆 Player 1 Wins!";
+}else if (randNum2 > randNum1) {
+  document.querySelector("h1").innerText = "Player 2 Wins!🏆 ";
+}else{
+  document.querySelector("h1").innerText = "🙅Draw!🙅";
+}
